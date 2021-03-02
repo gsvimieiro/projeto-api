72d32ee476158c74e1f0df77f11d9c14fad17266
gsvimieiro01 <gsvimieiro@gmail.com>
2021-03-01 21:18:56 -0300

    inicio projeto

---
+++ projeto-api/src/main/java/com/projeto/model/Cliente.java
@@ -0,0 +1,60 @@
+package com.projeto.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + (int) (id ^ (id >>> 32));
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Cliente other = (Cliente) obj;
+//		if (id != other.id)
+//			return false;
+//		return true;
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getNome() {
+//		return nome;
+//	}
+//
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+}
---
+++ projeto-api/src/main/java/com/projeto/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.projeto.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	Optional<Cliente> findById(Long id);
+
+}
